fe7ad54203fe96f5766ce67abff2fa6c72b87c28
Dantong Jin <j.dantong@gmail.com>
2026-08-14 09:48:14 +0800

    Initial Commit

---
+++ Deepseek Harness/Deepseek_HarnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deepseek_HarnessApp.swift
+//  Deepseek Harness
+//
+//  Created by Dantong Jin on 2026/8/14.
+//
+
+import SwiftUI
+
+@main
+struct Deepseek_HarnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
